936a7941d9a8f335c18a485dad5484d11835720f
lasse <lasse-winkel@t-online.de>
2024-08-18 10:31:28 +0200

    Create json data for different scenarios

--- moq_quality/vmaf.sh
+++ moq_quality/vmaf.sh
@@ -1,6 +1,31 @@
-ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_12_packet_loss.yuv \
-       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video.yuv \
-       -lavfi "libvmaf=log_fmt=json:log_path=idle_output_bbb.json:n_threads=8" \
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_naive.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_scenic.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_naive_output_48_vs_scenic.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_naive.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_48.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_naive_output_48_vs_48.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_adjusted.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_scenic.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_adjusted_output_48_vs_scenic.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_adjusted.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_48.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_adjusted_output_48_vs_48.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_packet_loss.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_scenic.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=packet_loss_output_48_vs_scenic.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_packet_loss.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_48.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=packet_loss_output_48_vs_48.json:n_threads=8" \
        -f null -
 
 
