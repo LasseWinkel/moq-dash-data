0f6b669e73ea83468e2051893f5d347111e4d0d1
lasse <lasse-winkel@t-online.de>
2024-08-21 09:19:09 +0200

    Update vmaf scores

--- moq_quality/vmaf.sh
+++ moq_quality/vmaf.sh
@@ -1,6 +1,21 @@
-ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_naive.yuv \
-       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_scenic.yuv \
-       -lavfi "libvmaf=log_fmt=json:log_path=idle_naive_output_48_vs_scenic.json:n_threads=8" \
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_12_idle_naive.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_12.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_naive_output_12_vs_12.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_12_idle_adjusted.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_12.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_adjusted_output_12_vs_12.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_24_idle_naive.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_24.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_naive_output_24_vs_24.json:n_threads=8" \
+       -f null -
+
+ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_24_idle_adjusted.yuv \
+       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_24.yuv \
+       -lavfi "libvmaf=log_fmt=json:log_path=idle_adjusted_output_24_vs_24.json:n_threads=8" \
        -f null -
 
 ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_naive.yuv \
@@ -9,23 +24,8 @@ ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i rec
        -f null -
 
 ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_adjusted.yuv \
-       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_scenic.yuv \
-       -lavfi "libvmaf=log_fmt=json:log_path=idle_adjusted_output_48_vs_scenic.json:n_threads=8" \
-       -f null -
-
-ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_idle_adjusted.yuv \
        -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_48.yuv \
        -lavfi "libvmaf=log_fmt=json:log_path=idle_adjusted_output_48_vs_48.json:n_threads=8" \
-       -f null -
-
-ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_packet_loss.yuv \
-       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_scenic.yuv \
-       -lavfi "libvmaf=log_fmt=json:log_path=packet_loss_output_48_vs_scenic.json:n_threads=8" \
-       -f null -
-
-ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i received_video_48_packet_loss.yuv \
-       -video_size 1280x720 -framerate 24 -pixel_format yuv420p -i trimmed_published_video_48.yuv \
-       -lavfi "libvmaf=log_fmt=json:log_path=packet_loss_output_48_vs_48.json:n_threads=8" \
        -f null -
 
 
@@ -36,6 +36,10 @@ ffmpeg -hide_banner -video_size 1280x720 -framerate 24 -pixel_format rgba -i rec
 # GoP 24 packet loss: 51.17
 # GoP 12 packet loss: 72.64
 
-# GoP 48 idle: 80.18
-# GoP 24 idle: 75.82
-# GoP 12 idle: 73.08
+# GoP 48 idle adjusted: 80.18
+# GoP 24 idle adjusted: 75.82
+# GoP 12 idle adjusted: 73.08
+
+# GoP 48 idle naive: 78.81
+# GoP 24 idle naive: 78.81
+# GoP 12 idle naive: 78.73
